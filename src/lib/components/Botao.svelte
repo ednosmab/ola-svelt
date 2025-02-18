f01ae8439f55196d21a2577ec0bcb1c048e98833
Edson Garcia <ednosmab@yahoo.com.br>
2025-02-18 19:51:04 -0300

    first button component

---
+++ src/lib/components/Botao.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let texto: string;
+</script>
+
+<button class="botao"> {texto} </button>
+
+<style>
+    .botao {
+        width: 100px;
+        height: 45px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Botao from "$lib/components/Botao.svelte";
+</script>
+
+<main>
+    <Botao texto="9" />
+</main>
