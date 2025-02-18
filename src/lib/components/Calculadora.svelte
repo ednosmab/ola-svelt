a00c2aff9d8b52e7d4d658474821ebe7a39edc9d
Edson Garcia <ednosmab@yahoo.com.br>
2025-02-18 20:11:07 -0300

    refactor for remove style css

---
+++ src/lib/components/Calculadora.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Botao from "./Botao.svelte";
+</script>
+
+<div class="calculadora">
+    <Botao texto="7" />
+    <Botao texto="8" />
+    <Botao texto="9" />
+</div>
+
+<style>
+    .calculadora {
+        display: flex;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,21 @@
 <script>
     import Botao from "$lib/components/Botao.svelte";
+    import Calculadora from "$lib/components/Calculadora.svelte";
 </script>
 
 <main>
-    <Botao texto="9" />
+    <Calculadora />
 </main>
+
+<style>
+    main {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    main {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+    }
+</style>
