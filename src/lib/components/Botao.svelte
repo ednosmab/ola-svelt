a3a50da550e4c4a5843be470f1219c2cabe1570a
Edson Garcia <ednosmab@yahoo.com.br>
2025-02-19 18:45:56 -0300

    add button component

--- src/lib/components/Botao.svelte
+++ src/lib/components/Botao.svelte
@@ -1,12 +1,34 @@
 <script lang="ts">
     export let texto: string;
+    export let triplo: boolean = false;
+    export let duplo: boolean = false;
+    export let operacao: boolean = false;
+    export let destaque: boolean = false;
 </script>
 
-<button class="botao"> {texto} </button>
+<button class="botao" class:triplo class:duplo class:operacao class:destaque>
+    {texto}
+</button>
 
 <style>
     .botao {
-        width: 100px;
-        height: 45px;
+        flex-basis: 25%;
+        height: 35px;
+        margin: 0;
+        border: 1px solid var(--cor-fundo);
+    }
+    .triplo {
+        flex-basis: 75%;
+    }
+    .duplo {
+        flex-basis: 50%;
+    }
+    .operacao {
+        background-color: orange;
+        color: white;
+    }
+    .destaque {
+        background-color: red;
+        color: white;
     }
 </style>
