55aa2656b20cddb00205ffa0b395ed30fd697ce8
Edson Garcia <ednosmab@yahoo.com.br>
2025-02-19 18:46:34 -0300

    add calculator component

--- src/lib/components/Calculadora.svelte
+++ src/lib/components/Calculadora.svelte
@@ -1,15 +1,49 @@
 <script lang="ts">
     import Botao from "./Botao.svelte";
+    import Linha from "./Linha.svelte";
 </script>
 
 <div class="calculadora">
-    <Botao texto="7" />
-    <Botao texto="8" />
-    <Botao texto="9" />
+    <Linha>
+        <Botao destaque triplo texto="AC" />
+        <Botao operacao texto="/" />
+    </Linha>
+    <Linha>
+        <Botao texto="7" />
+        <Botao texto="8" />
+        <Botao texto="9" />
+        <Botao operacao texto="*" />
+    </Linha>
+    <Linha>
+        <Botao texto="4" />
+        <Botao texto="5" />
+        <Botao texto="6" />
+        <Botao operacao texto="-" />
+    </Linha>
+    <Linha>
+        <Botao texto="1" />
+        <Botao texto="2" />
+        <Botao texto="3" />
+        <Botao operacao texto="+" />
+    </Linha>
+    <Linha>
+        <Botao duplo texto="0" />
+        <Botao texto="," />
+        <Botao destaque texto="=" />
+    </Linha>
 </div>
 
 <style>
+    :root {
+        --cor-fundo: #555;
+    }
     .calculadora {
+        width: 150px;
+        height: 220px;
         display: flex;
+        flex-direction: column;
+        background-color: var(--cor-fundo);
+        padding: 10px;
+        border-radius: 10px;
     }
 </style>
