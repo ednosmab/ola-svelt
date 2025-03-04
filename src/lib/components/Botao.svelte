d2ddb28f5ed62e3166ec66d1a5c571a601c5b678
Edson Garcia <ednosmab@yahoo.com.br>
2025-03-04 13:58:43 -0300

    add onClick function

--- src/lib/components/Botao.svelte
+++ src/lib/components/Botao.svelte
@@ -4,9 +4,17 @@
     export let duplo: boolean = false;
     export let operacao: boolean = false;
     export let destaque: boolean = false;
+    export let onClick: (valor: string) => void = () => {};
 </script>
 
-<button class="botao" class:triplo class:duplo class:operacao class:destaque>
+<button
+    class="botao"
+    class:triplo
+    class:duplo
+    class:operacao
+    class:destaque
+    on:click={() => onClick(texto)}
+>
     {texto}
 </button>
 
@@ -16,6 +24,7 @@
         height: 35px;
         margin: 0;
         border: 1px solid var(--cor-fundo);
+        cursor: pointer;
     }
     .triplo {
         flex-basis: 75%;
